c0d46641fa9308f7f56a03f2ec81c5b3d0d5c0df
HamedGhoochanian <hamedghoochanian@gmail.com>
2021-04-08 13:31:36 +0430

    added fetch user api

--- app/users/router.js
+++ app/users/router.js
@@ -2,4 +2,5 @@ const controllers = require('./controllers');
 
 module.exports = (app, router) => {
     router.post('/api/users', controllers.create);
+    router.post('/api/users/:id', controllers.fetch);
 };
---
+++ app/users/services/fetch.js
@@ -0,0 +1,13 @@
+const db = require('../../../core/db/postgresql');
+
+const fetchUser = async (id) => db.fetch({
+    text: `SELECT id, phone_number, first_name, last_name, is_active, is_verified, account_type_id
+           FROM users
+           WHERE is_deleted = true
+             AND id = $1`,
+    values: [id],
+});
+
+module.exports = {
+    fetchUser,
+};
