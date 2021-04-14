0de9a969e99cd1376a9a42bd14b06f779b59a86e
HamedGhoochanian <hamedghoochanain@gmail.com>
2021-04-14 22:39:40 +0430

    fixed error handler

--- core/util/errorHandler.js
+++ core/util/errorHandler.js
@@ -1,19 +1,23 @@
 const fs = require('fs');
 const dateformat = require('dateformat');
-const { error: errorResponse } = require('./response');
+const { error } = require('./response');
 
 // eslint-disable-next-line no-unused-vars
-exports.errorHandler = async ({ error, code }, req, res, next) => {
+exports.errorHandler = async (err, req, res, next) => {
     // eslint-disable-next-line no-console
+    // console.log(err);
+    let code = 500;
 
+    if (Object.prototype.hasOwnProperty.call(err, 'code') && Object.prototype.hasOwnProperty.call(err, 'error')) {
+        code = err.code;
+        // eslint-disable-next-line no-param-reassign
+        err = err.error;
+    }
     const date = dateformat(new Date(), 'yyyy-mm-dd');
     // Log the error
     fs.appendFileSync(`./logs/${date}.txt`,
-        `${new Date()}: ${error.stack ? error.stack : error}\n`, 'utf8');
+        `${new Date()}: ${err.stack ? err.stack : err}\n`, 'utf8');
 
     // Send 500 error to client
-    return errorResponse(res, code, {
-        fa: 'اتفاق ناخواسته ای سمت سرور رخ داد.',
-        en: 'Something unexpected happend.',
-    });
+    return error(res, code, { en: 'something unexpected happend.' });
 };
