f69e7ad14213d1f6354583dab6e01be0cae03677
Afarin Zamanian <afzado@gmail.com>
2021-04-17 10:40:18 +0430

    add account types

--- app/sql.sql
+++ app/sql.sql
@@ -163,4 +163,5 @@ CREATE TABLE IF NOT EXISTS reports
     FOREIGN KEY (activity_id) REFERENCES activities
 );
 
-INSERT INTO account_types (id, name) values (1, 'basic');
+INSERT INTO account_types (id, name, price) values (1, 'admin', 100) ON CONFLICT (id) DO UPDATE SET name = 'admin', price = 100;
+INSERT INTO account_types (id, name, price) values (2, 'member', 100) ON CONFLICT (id) DO UPDATE SET name = 'member', price = 100;
