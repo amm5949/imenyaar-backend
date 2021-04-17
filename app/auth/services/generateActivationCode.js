99f580c9ec8d651f170c82add9815fc2736d6e68
Afarin Zamanian <afzado@gmail.com>
2021-04-17 12:21:30 +0430

    create default activation code

--- app/auth/services/generateActivationCode.js
+++ app/auth/services/generateActivationCode.js
@@ -2,7 +2,8 @@ const db = require('../../../core/db/postgresql');
 const auth = require('../../../core/auth/auth');
 
 module.exports = (id) => {
-    const token = auth.genRandomString(8);
+    // const token = auth.genRandomString(8);
+    const token = '00000000';
     return db.insertQuery('activation_codes', {
         user_id: id,
         token,
