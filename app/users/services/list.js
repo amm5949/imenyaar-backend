ff1c36e4cbdb83481075ee0d0c9aae529fde3137
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-05-23 11:27:04 +0430

    return referer_id on list

--- app/users/services/list.js
+++ app/users/services/list.js
@@ -3,7 +3,7 @@ const db = require('../../../core/db/postgresql');
 
 const fetchUsers = async () => {
     const res = await db.executeQuery({
-        text: `SELECT u.id, u.first_name, u.last_name, u.phone_number, u.account_type_id, r.name as role, u.is_verified
+        text: `SELECT u.id, u.first_name, u.last_name, u.phone_number, u.account_type_id, r.name as role, u.is_verified, u.referer_id
                FROM users as u
                LEFT JOIN user_roles as ur on ur.user_id = u.id 
                LEFT JOIN roles as r on r.id = ur.role_id
