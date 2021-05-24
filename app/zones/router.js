69cdde22ed6d5064d7212fd1efbcdcca4bc1ef89
tooraj taraz <tooraj.info@gmail.com>
2021-05-24 16:53:19 +0430

    fix routes

--- app/zones/router.js
+++ app/zones/router.js
@@ -1,9 +1,9 @@
 const controllers = require('./controllers');
 
 module.exports = (app, router) => {
-    router.post('/api/projects', controllers.create);
-    router.get('/api/projects/:id', controllers.fetch);
-    router.get('/api/projects', controllers.list);
-    router.put('/api/projects/:id', controllers.update);
-    router.delete('/api/projects/:id', controllers.remove);
+    router.post('/api/zones', controllers.create);
+    router.get('/api/zones/:id', controllers.fetch);
+    router.get('/api/zones', controllers.list);
+    router.put('/api/zones/:id', controllers.update);
+    router.delete('/api/zones/:id', controllers.remove);
 };
