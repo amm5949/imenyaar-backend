30fde676bcf3f51e19705bf9f8a4b546980cc1c6
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-06-11 12:30:51 +0430

    fix typo

--- app/questions/controllers/image.js
+++ app/questions/controllers/image.js
@@ -7,14 +7,14 @@ const imageService = require('../services/image');
  * @apiVersion 1.0.0
  * @apiDescription Add an image to a question.
  * 
- * @apiParam (file) {file[]} images Image files to upload.
+ * @apiParam (file) images Image files to upload.
  */
 const addImage = async (request, response) => {
     const { id } = request.params;
     const images = Array.isArray(request.files.images) ? request.files.images
         : [request.files.images];
     const paths = images.map((i) => {
-        const path = `uploades/${id}-${i.name}`;
+        const path = `uploads/${id}-${i.name}`;
         i.mv(path);
         return path;
     });
