28cc8a8a139c5246c8a0e447f84da48274399970
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-06-11 12:24:08 +0430

    apidoc

--- app/questions/controllers/image.js
+++ app/questions/controllers/image.js
@@ -1,6 +1,14 @@
 const { ok } = require('../../../core/util/response');
 const imageService = require('../services/image');
-
+/**
+ * @api {post} /api/questions/:id/images AddImage
+ * @apiGroup Questions
+ * @apiName AddImage
+ * @apiVersion 1.0.0
+ * @apiDescription Add an image to a question.
+ * 
+ * @apiParam (file) {file[]} images Image files to upload.
+ */
 const addImage = async (request, response) => {
     const { id } = request.params;
     const images = Array.isArray(request.files.images) ? request.files.images
