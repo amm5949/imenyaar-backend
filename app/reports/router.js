6f442d571b30f9eb66a042f29b8a3ad64d757e15
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-07-11 21:32:18 +0430

    update report router

--- app/reports/router.js
+++ app/reports/router.js
@@ -2,6 +2,7 @@ const controllers = require('./controllers');
 
 module.exports = (app, router) => {
     router.post('/api/reports', controllers.create);
+    router.put('/api/reports/:id', controllers.update);
     router.get('/api/reports/:id', controllers.fetch);
     router.get('/api/reports', controllers.list);    
     router.post('/api/reports/:id/files', controllers.files);
