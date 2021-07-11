3c67f1121b5661b40348051b90e051ad62e66f2e
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-07-11 21:30:52 +0430

    add activity access check

--- app/activities/services/fetch.js
+++ app/activities/services/fetch.js
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 const db = require('../../../core/db/postgresql');
-
+// const projectCheckAccess = require('../../projects/services/checkAccess');
 
 const fetch_activity = async (id) => db.fetch({
     text: `SELECT id, start_date, scheduled_end_date, person_id, status, is_done
@@ -9,6 +9,20 @@ const fetch_activity = async (id) => db.fetch({
     values: [id],
 });
 
+const checkAccess = async (userId, activityId) => {
+
+    const projectId = (await db.fetch({
+        text: `SELECT project_id FROM activities a
+        WHERE a.id = $1`,
+        values: [activityId]
+    })).project_id;
+
+    const access = (await projectCheckAccess(userId, projectId));
+
+    return access;
+}
+
 module.exports = {
     fetch_activity,
+    checkAccess,
 };
