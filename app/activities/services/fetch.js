5686bf61c4b1d62e0cddcdca2123a3ccc2dd32f7
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-07-17 00:36:37 +0430

    fix access check

--- app/activities/services/fetch.js
+++ app/activities/services/fetch.js
@@ -2,10 +2,16 @@
 const db = require('../../../core/db/postgresql');
 
 const fetch_activity = async (id, user) => db.fetch({
-    text: `SELECT id, start_date, scheduled_end_date, project_id, people, zones, status, is_done
-           FROM activities 
-           WHERE is_deleted = false AND id = $1 AND ($2 in (select owner_id from projects where is_deleted=false and activities.project_id = projects.id) or $2 in (select user_id from user_roles u where u.role_id = 1 and is_deleted=false) or people @> $3 )`,
-    values: [id, user.id, [user.id]],
+    text: `SELECT a.*
+           FROM activities a
+           WHERE is_deleted = false 
+           AND id = $1 
+           AND (
+               $2 in (select owner_id from projects p where is_deleted=false and a.project_id = p.id) 
+               OR $2 in (select user_id from user_roles u where u.role_id = 1 and is_deleted=false) 
+               OR $2 = ANY(a.people)
+            )`,
+    values: [id, user.id],
 });
 
 module.exports = {
