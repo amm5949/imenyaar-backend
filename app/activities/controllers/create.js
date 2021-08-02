632763a832e4cdb09ffbe134b3f51b28609cee43
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-08-02 14:57:12 +0430

    add subscription check

--- app/activities/controllers/create.js
+++ app/activities/controllers/create.js
@@ -3,10 +3,11 @@ const validator = require('../../../core/util/validator');
 const { ok, error } = require('../../../core/util/response');
 const createSchema = require('../schemas/create');
 const accessCheck = require('../../projects/services/accessCheck.js');
+const subscriptionService = require('../../subscription/services/check.js');
 /**
  * @api {post} /api/activities create
- * @apiName CreateActivity
- * @apiGroup Activity
+ * @apiName Create Activity
+ * @apiGroup Activities
  * @apiVersion 1.0.0
  * @apiDescription Create an Activity
  *
@@ -17,7 +18,7 @@ const accessCheck = require('../../projects/services/accessCheck.js');
  * @apiParam {Date} start_date acceptable format is "new Date()" 
  * @apiParam {Date} scheduled_end_date acceptable format is "new Date()"
  *
- * @apiParamExample
+ * @apiParamExample {json} Request-Example:
   {
     "status": 10,
     "project_id" : 1,
@@ -26,6 +27,7 @@ const accessCheck = require('../../projects/services/accessCheck.js');
     "scheduled_end_date": "2021-07-14T14:53:20.896Z"
   }
  * @apiSuccessExample 
+  HTTP/1.1 200
 {
     "status": "ok",
     "message": {
@@ -47,18 +49,48 @@ const accessCheck = require('../../projects/services/accessCheck.js');
         "project_id": 1
     }
 }
+ * @apiError (403) Forbidden permission denied/activity limit reached.
+ * @apiErrorExample {json} Forbidden
+ * HTTP/1.1 403 Forbidden
+ * {
+	"status": "error",
+	"message": {
+		"en": "you don't have access to this project",
+		"fa": "شما به این پروژه دسترسی ندارید."
+	}
+ * }
+ * @apiErrorExample {json} Forbidden
+ * HTTP/1.1 403 Forbidden
+ * {
+	"status": "error",
+	"message": {
+		"en": "you have reached your activity limit.",
+		"fa": "شما به سقف فعالیت‌های مجاز رسیده‌اید."
+	}
+ * }
+ * @apiError (400) BadRequest zone/people not provided
  */
 
 const create = async (request, response) => {
     const result = validator(createSchema, request.body);
+    const user = request.user;
 
     if (result.failed) {
         return result.response(response);
     }
 
-    if (!(await accessCheck(request.user, result.data.project_id))) {
+    if (!(await accessCheck(user, result.data.project_id))) {
         return error(response, 403, {
             en: 'you don\'t have access to this project',
+            fa: 'شما به این پروژه دسترسی ندارید.'
+        });
+    }
+    if (user.roles[0].name !== 'admin' && !(await 
+        subscriptionService.checkByManager(user.id, 'activity', result.data.project_id)
+        )){
+        return error(response, 403, {
+            en: 'you have reached your activity limit.',
+            fa: 'شما به سقف فعالیت‌های مجاز رسیده‌اید.'
         });
     }
     const activity_details = result.data;
@@ -68,7 +100,7 @@ const create = async (request, response) => {
             en: 'no acceptable zone',
         });
     }
-    if (!Object.prototype.hasOwnProperty.call(activity_details, 'people')) {
+    if (!activity_details.hasOwnProperty('people')) {
         activity_details.people = [request.user.id];
     }
     activity_details.people = await createService.test_users(activity_details.people); 
