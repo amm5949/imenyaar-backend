676d45b89f4188c327c8cef1e9f2136b87834b5d
Afarin Zamanian <23293389+Af4rinz@users.noreply.github.com>
2021-08-04 20:58:12 +0430

    add subscription history to fetch

--- app/users/controllers/fetch.js
+++ app/users/controllers/fetch.js
@@ -7,25 +7,68 @@ const { ok, error } = require('../../../core/util/response');
  * @apiGroup User
  * @apiVersion 1.0.0
  * @apiDescription Fetch a user
- * @apiSuccess {Number} id
- * @apiSuccess {String} phone_number
- * @apiSuccess {String} last_name
- * @apiSuccess {String} first_name
- * @apiSuccess {Boolean} is_active
- * @apiSuccess {Boolean} is_verified
+ * @apiSuccess {Object} result.user User information
+ * @apiSuccess {Array} [result.subscriptions] User subscription information 
+ *                                           (only if user is admin or request themselves)
+ * @apiSuccessExample {json} Success-Response (for users with full access):
+ * HTTP/1.1 200 OK
+ * 	"status": "ok",
+	"message": {
+		"en": "Request was successful",
+		"fa": "درخواست موفقیت آمیز بود"
+	},
+	"result": {
+		"user": {
+			"id": 12,
+			"phone_number": "09111234567",
+			"first_name": "Tuesday",
+			"last_name": "Weld",
+			"is_active": true,
+			"is_verified": true
+		},
+		"subscriptions": [
+			{
+				"id": 25,
+				"user_id": 12,
+				"account_type_id": 1,
+				"start_date": "2022-08-03T17:48:02.241Z",
+				"end_date": "2023-08-03T17:49:10.559Z",
+				"cost": 10000,
+				"is_verified": true,
+				"authority": "000000000000000000000000000000571822"
+			},
+			{
+				"id": 23,
+				"user_id": 12,
+				"account_type_id": 2,
+				"start_date": "2021-08-03T17:48:02.241Z",
+				"end_date": "2022-08-03T17:48:02.241Z",
+				"cost": 10000,
+				"is_verified": true,
+				"authority": "000000000000000000000000000000571819"
+			}
+		]
+	}
+}
+ * @apiSuccessExample {json} Success-Response (for users without access):
+ * HTTP/1.1 200 OK
+ * 
+ *        
+ *  
  *
  */
 
 const fetch = async (request, response) => {
     const { id } = request.params;
-    const user = await fetchService.fetchUser(id);
-    if (!user) {
+    let hasAccess = (request.user.id === id || request.user.roles[0].name ==='admin');
+    const res = await fetchService.fetchUser(id, hasAccess);
+    if (!res) {
         return error(response, 404, {
             en: 'User not found.',
             fa: 'کاربر یافت نشد.',
         });
     }
-    return ok(response, user, { }, 200);
+    return ok(response, res, {}, 200);
 };
 
 module.exports = async (request, response, ) => {
--- app/users/services/fetch.js
+++ app/users/services/fetch.js
@@ -1,12 +1,25 @@
 const db = require('../../../core/db/postgresql');
 
 
-const fetchUser = async (id) => db.fetch({
-    text: `SELECT id, phone_number, first_name, last_name, is_active, is_verified
-           FROM users
-           WHERE is_deleted = false AND id = $1`,
-    values: [id],
-});
+const fetchUser = async (id, hasAccess) => {
+    const user = await db.fetch({
+        text: `SELECT id, phone_number, first_name, last_name, is_active, is_verified
+            FROM users
+            WHERE is_deleted = false AND id = $1`,
+        values: [id],
+    });
+    if (!user || !hasAccess) {
+        return {...user};
+    }
+    const subscriptionData = await db.fetchAll({
+        text: `SELECT * FROM subscriptions WHERE user_id = $1`,
+        values: [id]
+    });
+    return {
+        user: user,
+        subscriptions: subscriptionData,
+    };
+}
 
 module.exports = {
     fetchUser,
